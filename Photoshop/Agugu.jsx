4bd2fa41de7199c45f40b1d99379c9520d3a826b
FrankNine <aaefiikmnnnr@gmail.com>
2016-10-29 16:49:57 +0800

    Created a Window with a button, so many loopholes

---
+++ Photoshop/Agugu.jsx
@@ -0,0 +1,7 @@
+﻿var mainWindow = new Window ("dialog", "Agugu");
+
+mainWindow.frameLocation = [400,160];
+mainWindow.size = [200, 200];
+mainWindow.btnPnl = mainWindow.add("panel", [10,10,180,180], "Export");
+mainWindow.btnPnl.add("button", [10,10,80,80], "OK");
+mainWindow.show();
