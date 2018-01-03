453af79544d0599ac5cd46f1cfb54e6e9fdfacc2
Frank Chao <aaefiikmnnnr@gmail.com>
2018-01-03 14:28:38 +0800

    feat: Get PSD path from selected Asset and list layers of it
    Also adds a test case PSD

---
+++ Unity/Agugu/Assets/Agugu/Editor/Importer/PSDImporter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+using UnityEngine;
+using UnityEditor;
+
+using Ntreev.Library.Psd;
+
+
+public class PSDImporter
+{
+    [MenuItem("Agugu/Import Selection")]
+    public static void ImportSelection()
+    {
+        UnityEngine.Object selectedObject = Selection.activeObject;
+        string selectedObjectPath = AssetDatabase.GetAssetPath(selectedObject);
+
+        string fileExtension = Path.GetExtension(selectedObjectPath);
+        bool isPsdFile = string.Equals(fileExtension, ".psd", 
+                                       StringComparison.OrdinalIgnoreCase);
+
+        if (!isPsdFile)
+        {
+            Debug.LogError("Selected Asset is not a PSD file");
+            return;
+        }
+
+        using (var document = PsdDocument.Create(selectedObjectPath))
+        {
+            foreach (var item in document.Childs)
+            {
+                Debug.Log("LayerName : " + item.Name);
+            }
+        }
+    }
+}
+
