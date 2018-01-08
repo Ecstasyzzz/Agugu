8b2155e929ae758300609211a5163d031f0a8012
Frank Chao <aaefiikmnnnr@gmail.com>
2018-01-08 17:55:52 +0800

    feat: List all layer in Listbox UI
    TreeView not working in Photoshop CC 2018, need to figure it out

--- Photoshop/Agugu.jsx
+++ Photoshop/Agugu.jsx
@@ -1,7 +1,35 @@
-﻿var mainWindow = new Window ("dialog", "Agugu");
+﻿#target photoshop
+
+
+function getAllLayerNames(doc){
+	var result = [];
+	for (var i = 0; i < doc.layers.length; i++)  
+	{
+		result.push(doc.layers[i].name)
+	}  
+	return result;
+}
+
+
+var mainWindow = new Window ("palette", "Agugu");
+mainWindow.orientation = "row";
 
 mainWindow.frameLocation = [400,160];
-mainWindow.size = [200, 200];
-mainWindow.btnPnl = mainWindow.add("panel", [10,10,180,180], "Export");
-mainWindow.btnPnl.add("button", [10,10,80,80], "OK");
+mainWindow.layerGroup = mainWindow.add("group");
+mainWindow.layerGroup.layerList = mainWindow.layerGroup.add ("listbox", [0, 0, 150, 250]);
+
+var allLayerNames = getAllLayerNames(activeDocument);
+for(layerName in allLayerNames)
+{
+	mainWindow.layerGroup.layerList.add ("item", allLayerNames[layerName]);
+}
+
+mainWindow.optionGroup = mainWindow.add("group");
+mainWindow.optionGroup.buttonPanel = mainWindow.optionGroup.add("panel", undefined, "Export");
+mainWindow.optionGroup.buttonPanel.okButton = mainWindow.optionGroup.buttonPanel.add("button", undefined, "OK");
+
 mainWindow.show();
+
+while(true){
+	app.refresh();
+}
