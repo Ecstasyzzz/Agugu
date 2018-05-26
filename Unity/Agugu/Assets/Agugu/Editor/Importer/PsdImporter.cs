0e6c22a6a1b1c7b307415f347b72e025d2e81b4e
FrankNine <aaefiikmnnnr@gmail.com>
2018-05-27 00:00:10 +0800

    fix: Object reference retargeting

--- Unity/Agugu/Assets/Agugu/Editor/Importer/PsdImporter.cs
+++ Unity/Agugu/Assets/Agugu/Editor/Importer/PsdImporter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.IO;
+using System.Collections;
+using System.Collections.Generic;
 
 using UnityEngine;
 using UnityEngine.UI;
@@ -179,23 +180,176 @@ public class PsdImporter
         }
         else
         {
-            var components = (prefabObject as GameObject).GetComponents<Component>();
-            foreach (var component in components)
-            {
-                if (!(component is Graphic) &&
-                    !(component is Canvas) &&
-                    !(component is CanvasScaler) &&
-                    !(component is RectTransform) &&
-                    !(component is GraphicRaycaster))
-                {
-                    UnityEditorInternal.ComponentUtility.CopyComponent(component);
-                    UnityEditorInternal.ComponentUtility.PasteComponentAsNew(uiGameObject);
-                }
-            }
+            var prefabInstance = GameObject.Instantiate(prefabObject) as GameObject;
+            _MigrateAppliedPrefabModification(prefabInstance, uiGameObject);
+            GameObject.DestroyImmediate(prefabInstance);
         }
 
         PrefabUtility.ReplacePrefab(uiGameObject, prefabObject, ReplacePrefabOptions.ReplaceNameBased);
     }
+
+    private static void _MigrateAppliedPrefabModification
+    (
+        GameObject sourceGameObjectRoot,
+        GameObject targetGameObjectRoot
+    )
+    {
+        Dictionary<int, GameObject> sourceMapping =
+            _BuildLayerIdToGameObjectMapping(sourceGameObjectRoot);
+        Dictionary<int, GameObject> targetMapping =
+            _BuildLayerIdToGameObjectMapping(targetGameObjectRoot);
+
+        _MoveNonImportedGameObjects(sourceGameObjectRoot, targetGameObjectRoot);
+
+        foreach (var idAndGameObjectPair in targetMapping)
+        {
+            if (sourceMapping.ContainsKey(idAndGameObjectPair.Key))
+            {
+                GameObject source = sourceMapping[idAndGameObjectPair.Key];
+                GameObject target = idAndGameObjectPair.Value;
+
+                _MoveNonImportedGameObjects(source, target);
+            }
+        }
+
+        _CopyNonImportedComponents(sourceGameObjectRoot, targetGameObjectRoot, targetMapping);
+
+        foreach (var idAndGameObjectPair in targetMapping)
+        {
+            if (sourceMapping.ContainsKey(idAndGameObjectPair.Key))
+            {
+                GameObject source = sourceMapping[idAndGameObjectPair.Key];
+                GameObject target = idAndGameObjectPair.Value;
+               
+                _CopyNonImportedComponents(source, target, targetMapping);
+            }
+        }
+    }
+
+    private static Dictionary<int, GameObject> _BuildLayerIdToGameObjectMapping(GameObject root)
+    {
+        var psdLayerIdTagList = root.GetComponentsInChildren<PsdLayerIdTag>();
+
+        var mapping = new Dictionary<int, GameObject>();
+        foreach (var layerIdTag in psdLayerIdTagList)
+        {
+            mapping.Add(layerIdTag.LayerId, layerIdTag.gameObject);
+        }
+
+        return mapping;
+    }
+
+    private static void _CopyNonImportedComponents
+    (
+        GameObject source, 
+        GameObject target,
+        Dictionary<int, GameObject> targetLayerIdGameObjectMapping
+    )
+    {
+        var components = source.GetComponents<Component>();
+        foreach (var component in components)
+        {
+            if (!(component is Graphic) &&
+                !(component is CanvasRenderer) &&
+                !(component is Canvas) &&
+                !(component is CanvasScaler) &&
+                !(component is RectTransform) &&
+                !(component is GraphicRaycaster) &&
+                !(component is PsdLayerIdTag))
+            {
+                UnityEditorInternal.ComponentUtility.CopyComponent(component);
+                var targetComponent = target.AddComponent(component.GetType());
+                UnityEditorInternal.ComponentUtility.PasteComponentValues(targetComponent);
+                _RetargetObjectReference(component, targetComponent, targetLayerIdGameObjectMapping);
+            }
+        }
+    }
+
+    private static void _RetargetObjectReference
+    (
+        Component                   source,
+        Component                   target,
+        Dictionary<int, GameObject> targetLayerIdGameObjectMapping
+    )
+    {
+        var sourceSO = new SerializedObject(source);
+
+        var targetSO = new SerializedObject(target);
+        SerializedProperty targetSPIter = targetSO.GetIterator();
+        bool isIteratorValid = targetSPIter.Next(true);
+        while (isIteratorValid)
+        {
+            if (targetSPIter.propertyType == SerializedPropertyType.ObjectReference &&
+                !string.Equals(targetSPIter.name, "m_PrefabParentObject") &&
+                !string.Equals(targetSPIter.name, "m_PrefabInternal") &&
+                !string.Equals(targetSPIter.name, "m_GameObject") &&
+                !string.Equals(targetSPIter.name, "m_Script"))
+            {
+                string propertyPath = targetSPIter.propertyPath;
+                var sourceSP = sourceSO.FindProperty(propertyPath);
+                if (sourceSP != null)
+                {
+                    UnityEngine.Object sourceObjectReference = sourceSP.objectReferenceValue;
+                    GameObject sourceReferencedGameObject;
+                    if (sourceObjectReference is GameObject)
+                    {
+                        sourceReferencedGameObject = sourceObjectReference as GameObject;
+                    }
+                    else if(sourceObjectReference is Component)
+                    {
+                        sourceReferencedGameObject = (sourceObjectReference as Component).gameObject;
+                    }
+                    else
+                    {
+                        sourceReferencedGameObject = null;
+                    }
+
+                    if (sourceReferencedGameObject != null)
+                    {
+                        var sourceLayerIdTag = sourceReferencedGameObject.GetComponent<PsdLayerIdTag>();
+                        if (sourceLayerIdTag != null)
+                        {
+                            int layerId = sourceLayerIdTag.LayerId;
+
+                            if (targetLayerIdGameObjectMapping.ContainsKey(layerId))
+                            {
+                                if (sourceObjectReference is GameObject)
+                                {
+                                    targetSPIter.objectReferenceValue = targetLayerIdGameObjectMapping[layerId];
+                                }
+                                else if (sourceObjectReference is Component)
+                                {
+                                    GameObject retargetGameObject = targetLayerIdGameObjectMapping[layerId];
+                                    Component retargetComponent =
+                                        retargetGameObject.GetComponent(sourceObjectReference.GetType());
+                                    targetSPIter.objectReferenceValue = retargetComponent;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            isIteratorValid = targetSPIter.Next(false);
+        }
+
+        targetSO.ApplyModifiedPropertiesWithoutUndo();
+    }
+
+    private static void _MoveNonImportedGameObjects(GameObject source, GameObject target)
+    {
+        var gameObjectsShouldMove = new List<GameObject>();
+        foreach (Transform child in source.transform)
+        {
+            if (child.GetComponent<PsdLayerIdTag>() == null)
+            {
+                gameObjectsShouldMove.Add(child.gameObject);
+            }
+        }
+
+        gameObjectsShouldMove.ForEach(go => go.transform.SetParent(target.transform));
+    }
+
 
 
     public static GameObject _BuildUguiGameObject(UiTreeRoot uiTree)
